f7f804a31e7181bbae82612ce8d9d266bfa775d4
Chae Sang, Jung <shtingstar@hanmail.net>
2020-11-25 23:19:18 +0900

    #9.3 Recap and Improvements

--- context_intro/src/Screen.js
+++ context_intro/src/Screen.js
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import Header from "./Header";
-import { UserContext } from "./context";
+import { useFns } from "./context";
 
 const Screen = () => {
-  const { logUserIn } = useContext(UserContext);
-  
+  const { logUserIn } = useFns();
+
   return (
     <div>
       <Header />
